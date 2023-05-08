d226b4b792723bbf76f996f406d36e36c1ff9325
Dariusz Legizynski <d.legizynski@.com>
2023-05-08 17:49:31 +0200

    pass params from front to back

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from 'react'
+
 import ListHeader from "./components/ListHeader"
 import "./App.css"
 
-function App() {
+const App = () => {
+  const userEmail = 'd.legizynski@gmail.com'
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+      const json = await response.json()
+      console.log(json)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => getData, [])
 
   return (
     <div className="app">
--- server/server.js
+++ server/server.js
@@ -1,18 +1,21 @@
 import "dotenv/config.js"
 import express from 'express'
+import cors from 'cors'
 
 import { pool } from "./db.js"
 
 const PORT = process.env.PORT ?? 8000
 const app = express()
 
+app.use(cors())
+
 app.get('/', (req, res) => {
   res.send('hello Dark')
 })
 
 // get all todos
-app.get('/todos', async (req, res) => {
-  const userEmail = "d.legizynski@gmail.com"
+app.get('/todos/:userEmail', async (req, res) => {
+  const { userEmail } = req.params
 
   try {
     const todos = await pool.query("SELECT * FROM todos WHERE user_email = $1", [userEmail])
