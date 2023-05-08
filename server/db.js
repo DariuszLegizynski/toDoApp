30947332612d66a97713ea08d9939603e28dff8b
Dariusz Legizynski <d.legizynski@.com>
2023-05-08 15:36:49 +0200

    connection to sql db working

--- server/db.js
+++ server/db.js
@@ -1,11 +1,8 @@
 import pg from 'pg'
 
-console.log(process.env.USERNAME)
-console.log(process.env.PASSWORD)
 export const pool = new pg.Pool({
-    user: 'postgres',
+    user: "postgres",
     password: process.env.PASSWORD,
-    // password: "",
     host: process.env.HOST,
     port: process.env.DBPORT,
     database: 'todoapp'
--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
+import "dotenv/config.js"
 import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
 
 import { pool } from "./db.js"
 
