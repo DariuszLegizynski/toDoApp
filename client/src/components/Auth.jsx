f70ed0ba3c8de6f8c0359cf484bd1e03831a21a1
Dariusz Legizynski <d.legizynski@.com>
2023-05-10 22:35:22 +0200

    add cookies

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,32 +1,66 @@
-import { useState } from 'react'
+import { useState } from "react"
+import { useCookies } from "react-cookie"
 
 const Auth = () => {
-  const [ isLogIn, setIsLogIn ] = useState(true)
-  const [ error, setError ] = useState("")
+  const [cookies, setCookie, removeCookie] = useCookies()
+	const [isLogIn, setIsLogIn] = useState(true)
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+	const [confirmPassword, setConfirmPassword] = useState("")
+	const [error, setError] = useState("")
 
-const viewLogin = status => {
-  setError("")
-  setIsLogIn(status)
-}
+  console.log(cookies)
 
-  return (
-    <article className="auth-container">
-      <section className="auth-conatiner-box">
-        <form>
-          <h2>{ isLogIn ? "Please log in" : "Please sign up"}</h2>
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
-          {!isLogIn && <input type="password"  placeholder="confirm password" />}
-          <input type="submit" className="create" />
-          {error && <p>{error}</p>}
-        </form>
-        <div className="auth-options">
-          <button onClick={() => viewLogin(false)} style={{backgroundColor : !isLogIn ? 'rgb(255. 255, 255)' : 'rgb(188, 188, 188'}}>Sign Up</button>
-          <button onClick={() => viewLogin(true)} style={{backgroundColor : isLogIn ? 'rgb(255. 255, 255)' : 'rgb(188, 188, 188'}}>Login</button>
-        </div>
-      </section>
-    </article>
-  )
+	const viewLogin = status => {
+		setError("")
+		setIsLogIn(status)
+	}
+
+	const handleSubmit = async (e, endpoint) => {
+		e.preventDefault()
+
+		if (!isLogIn && password !== confirmPassword) {
+			setError("Make sure passwords match!")
+			return
+		}
+
+		const response = await fetch(`http://localhost:8000/${endpoint}`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ email, password }),
+		})
+		const data = await response.json()
+
+    if(data.detail) {
+      setError(data.detail)
+    } else {
+      setCookie('Email', data.email)
+      setCookie('AuthToken', data.token)
+    }
+	}
+
+	return (
+		<article className="auth-container">
+			<section className="auth-conatiner-box">
+				<form>
+					<h2>{isLogIn ? "Please log in" : "Please sign up"}</h2>
+					<input type="email" placeholder="email" onChange={e => setEmail(e.target.value)} />
+					<input type="password" placeholder="password" onChange={e => setPassword(e.target.value)} />
+					{!isLogIn && <input type="password" placeholder="confirm password" onChange={e => setConfirmPassword(e.target.value)} />}
+					<input type="submit" className="create" onClick={e => handleSubmit(e, isLogIn ? "login" : "signup")} />
+					{error && <p>{error}</p>}
+				</form>
+				<div className="auth-options">
+					<button onClick={() => viewLogin(false)} style={{ backgroundColor: !isLogIn ? "rgb(255. 255, 255)" : "rgb(188, 188, 188" }}>
+						Sign Up
+					</button>
+					<button onClick={() => viewLogin(true)} style={{ backgroundColor: isLogIn ? "rgb(255. 255, 255)" : "rgb(188, 188, 188" }}>
+						Login
+					</button>
+				</div>
+			</section>
+		</article>
+	)
 }
 
 export default Auth
